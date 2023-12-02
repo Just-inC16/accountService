d15834c8568cde4ef8f799f51b5f699e4b053376
Justin Chan <justin.chan.3719@gmail.com>
2023-12-02 12:27:02 -0500

    Simple code base using restcontroller

--- src/main/java/com/tcs/accountService/AccountController.java
+++ src/main/java/com/tcs/accountService/AccountController.java
@@ -10,20 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 	@Autowired
 	private AccountRepository accountRepository;
-	@Autowired
-	private EventPublisher eventPublisher;
 
 	@GetMapping
 	public String getHelloWorld() {
 		return "Hello world! :)";
-	}
-
-	@GetMapping("/publish-event")
-	public String publishEvent() {
-		Account acc = new Account((long) 1, 29.99);
-		StartTransactionEvent event = new StartTransactionEvent(acc);
-		eventPublisher.publishStartTransactionEvent(event);
-		return "Event Publisher";
 	}
 
 }
